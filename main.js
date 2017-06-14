f0db3802d95eaebee6e2f71067fbd7dcc953b66a
Ivan Rossi <ivan.rossi95@gmail.com>
2017-06-14 18:25:33 +0200

    All numbers added
    TODO: change localshortcut with keypressed event

--- main.js
+++ main.js
@@ -26,15 +26,58 @@ function newWindow() {
   })
   win.webContents.openDevTools()
 
-  localShortcut.register(win,'1', () => {
+  calculatorShortcut(win)
+}
+
+function calculatorShortcut(w) {
+
+  localShortcut.register(w,'1', () => {
     win.webContents.send('scmex', '#n1');
-    console.log("Main listen to key 1");
   });
-  localShortcut.register(win,'2', () => {
+
+  localShortcut.register(w,'2', () => {
     win.webContents.send('scmex','#n2')
-    console.log("Main 2");
   })
 
+  localShortcut.register(w,'3', () => {
+    win.webContents.send('scmex','#n3')
+  })
+
+  localShortcut.register(w,'4', () => {
+    win.webContents.send('scmex','#n4')
+  })
+
+  localShortcut.register(w,'5', () => {
+    win.webContents.send('scmex','#n5')
+  })
+
+  localShortcut.register(w,'6', () => {
+    win.webContents.send('scmex','#n6')
+  })
+
+  localShortcut.register(w,'7', () => {
+    win.webContents.send('scmex','#n7')
+  })
+
+  localShortcut.register(w,'8', () => {
+    win.webContents.send('scmex','#n8')
+  })
+
+  localShortcut.register(w,'9', () => {
+    win.webContents.send('scmex','#n9')
+  })
+
+  localShortcut.register(w,'0', () => {
+    win.webContents.send('scmex','#n0')
+  })
+
+  localShortcut.register(w,'5', () => {
+    win.webContents.send('scmex','#n5')
+  })
+
+  localShortcut.register(w,'+', () => {
+    win.webContents.send('scmex','#plus')
+  })
 }
 
 app.on("ready",() => {
